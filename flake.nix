bf92920669d28773a95e9503ec3ade19b45f7872
qxrein <manavrj.07@gmail.com>
2025-05-31 02:11:36 +0530

    removed bazel build system and fixed flake

--- flake.nix
+++ flake.nix
@@ -1,80 +1,64 @@
 {
-  description = "Red Browser - GPU Accelerated Browser";
+  description = "Red Browser - GPU Accelerated Renderer";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    zig-overlay = {
-      url = "github:mitchellh/zig-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
+    zig-overlay.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, zig-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, zig-overlay, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            rust-overlay.overlays.default
-            zig-overlay.overlays.default
-          ];
+          overlays = [ zig-overlay.overlays.default ];
           config.allowUnfree = true;
         };
 
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" "rustfmt" "clippy" ];
-        };
-
-        zigToolchain = zig-overlay.packages.${system}."0.12.0";
-
-        nativeBuildInputs = with pkgs; [
-          rustToolchain
-          zigToolchain
-          bazel_6
-          bazel-buildtools
-          pkg-config
-        ];
+        zigToolchain = zig-overlay.packages.${system}."0.14.0";
 
         buildInputs = with pkgs; [
           glfw
-          vulkan-loader
+          freetype
+          harfbuzz
+          libglvnd  
           libxkbcommon
           wayland
           xorg.libX11
           xorg.libXcursor
           xorg.libXrandr
           xorg.libXi
+          zlib
         ];
+
       in
       {
-        devShells.default = pkgs.mkShell {
-          inherit nativeBuildInputs buildInputs;
-          
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-          
-          shellHook = ''
-            export PATH="${rustToolchain}/bin:${zigToolchain}/bin:$PATH"
-            echo "Rust: $(rustc --version)"
-            echo "Zig: $(zig version)"
-          '';
-        };
-
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "red-browser";
+        packages.default = pkgs.stdenvNoCC.mkDerivation {
+          name = "red-gpu";
           src = self;
-          inherit nativeBuildInputs buildInputs;
+          nativeBuildInputs = [ zigToolchain pkgs.pkg-config ];
+          buildInputs = buildInputs;
+          
+          LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
           
           buildPhase = ''
             export HOME=$(mktemp -d)
-            bazel build //:red-browser
+            zig build -Doptimize=ReleaseSafe --prefix $out
           '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ zigToolchain pkgs.pkg-config ];
+          buildInputs = buildInputs;
           
-          installPhase = ''
-            mkdir -p $out/bin
-            cp bazel-bin/red-browser $out/bin/
+          LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          
+          shellHook = ''
+            export PATH="${zigToolchain}/bin:$PATH"
+            echo "Zig: $(zig version)"
           '';
         };
 
